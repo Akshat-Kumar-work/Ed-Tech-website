f93d05f60860d4637288794f6b3f5be517ea9017
Akshat <iamakku0.0.0.1@gmail.com>
2023-07-25 21:08:38 +0530

    courseapi operation and slice

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice"
 import profileReducer from "../slices/profileSlice";
 import cartReducer from "../slices/cartSlice"
+import courseReducer from "../slices/courseSlice";
 
 //this rootReducer contain all reducer 
 const rootReducer = combineReducers({
     auth:authReducer,
     profile:profileReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    course:courseReducer,
 
 })
 
