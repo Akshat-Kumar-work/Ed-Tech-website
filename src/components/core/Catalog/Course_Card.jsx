00405128850442b53edebcd9adf3d4e4acf58c24
Akshat <iamakku0.0.0.1@gmail.com>
2023-07-30 00:15:17 +0530

    catalog done styling pending

--- src/components/core/Catalog/Course_Card.jsx
+++ src/components/core/Catalog/Course_Card.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import RatingStars from "../../common/RatingStar"
+import { useEffect } from 'react';
+import GetAvgRating from "../../../utils/avgRating"
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Course_Card = () => {
+
+const Course_Card = ({course , Height}) => {
+
+    const [avgReviewCount , setAvgReviewCount] = useState(0);
+
+    useEffect( ()=>{
+        const count = GetAvgRating(course.ratingAndReviews);
+        setAvgReviewCount(count)
+    },[course])
   return (
-    <div>Course_Card</div>
+    <div>
+        <Link to={`/courses/${course._id}`}>
+            <div>
+
+                <div>
+                    <img src={course?.thumbnail} alt='course thumbnail' className={`${Height} w-full rounded-xl object-cover`}/>
+                </div>
+                
+                <div>
+
+                    <p>{course?.courseName}</p>
+                    <p>{course?.instructor.firstName} {course?.instructor.lastName}</p>
+                
+
+                <div className='flex gap-x-3'>
+                    <span>{avgReviewCount || 0}</span>
+                    <RatingStars Review_Count={avgReviewCount} />
+                    <span>{course.ratingAndReviews.length} Ratings</span>
+                </div>
+
+                <p>{course?.price}</p>
+
+                </div>
+
+            </div>
+        </Link>
+    </div>
   )
 }
 
