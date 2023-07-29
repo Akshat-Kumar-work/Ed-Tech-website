20010d5dd2fff0fd983a6a48843be101785183e8
Akshat <iamakku0.0.0.1@gmail.com>
2023-07-29 12:21:01 +0530

    categories done

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -36,8 +36,7 @@ exports.createCategory = async(req, res)=>{
 exports.showAllCategories = async (req, res) => {
 	try {
 		const allCategorys = await Category.find(
-			{},
-			{ name: true, description: true }
+			{}
 		);
 		res.status(200).json({
 			success: true,
@@ -52,63 +51,78 @@ exports.showAllCategories = async (req, res) => {
 };
 
 //category page details
-exports.categoryPageDetails = async(req ,res)=>{
-    try{
-        const {categoryId} = req.body;
-
-        //get courses for the specified category
-        const selectedCategory = await Category.findById(categoryId).populate("courses").exec();
-
-        // Handle the case when the category is not found
-		if (!selectedCategory) {
-			console.log("Category not found.");
-			return res
-				.status(404)
-				.json({ success: false, message: "Category not found" });
-		}
-
-
-		// Handle the case when there are no courses inside selected category
-		if (selectedCategory.courses.length === 0) {
-			console.log("No courses found for the selected category.");
-			return res.status(404).json({
-				success: false,
-				message: "No courses found for the selected category.",
-			});
-		}
-
-        //fetchig the course available or present in selected category
-		const selectedCourses = selectedCategory.courses;
-
-        //get courses for other category
-        const categoriesExceptSelected = await Category.find({ _id: {$ne:categoryId} }).populate("courses");
-
-        //need to understand from here
-        let differentCourses = [];
-		
-		for (const category of categoriesExceptSelected) {
-			differentCourses.push(...category.courses);
-		}
-
-		// Get top-selling courses across all categories
-		const allCategories = await Category.find().populate("courses");
-		const allCourses = allCategories.flatMap((category) => category.courses);
-		const mostSellingCourses = allCourses
-			.sort((a, b) => b.sold - a.sold)
-			.slice(0, 10);
-
-		res.status(200).json({
-			selectedCourses: selectedCourses,
-			differentCourses: differentCourses,
-			mostSellingCourses: mostSellingCourses,
-		});
-    
+exports.categoryPageDetails = async (req, res) => {
+    try {
+      const { categoryId } = req.body
+      console.log("PRINTING CATEGORY ID: ", categoryId);
+      // Get courses for the specified category
+      const selectedCategory = await Category.findById(categoryId)
+        .populate({
+          path: "courses",
+          match: { status: "Published" },
+          populate: "ratingAndReviews",
+        })
+        .exec()
+  
+      //console.log("SELECTED COURSE", selectedCategory)
+      // Handle the case when the category is not found
+      if (!selectedCategory) {
+        console.log("Category not found.")
+        return res
+          .status(404)
+          .json({ success: false, message: "Category not found" })
+      }
+      // Handle the case when there are no courses
+      if (selectedCategory.courses.length === 0) {
+        console.log("No courses found for the selected category.")
+        return res.status(404).json({
+          success: false,
+          message: "No courses found for the selected category.",
+        })
+      }
+  
+      // Get courses for other categories
+      const categoriesExceptSelected = await Category.find({
+        _id: { $ne: categoryId },
+      })
+      let differentCategory = await Category.findOne(
+        categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+          ._id
+      )
+        .populate({
+          path: "courses",
+          match: { status: "Published" },
+        })
+        .exec()
+        //console.log("Different COURSE", differentCategory)
+      // Get top-selling courses across all categories
+      const allCategories = await Category.find()
+        .populate({
+          path: "courses",
+          match: { status: "Published" },
+          populate: {
+            path: "instructor",
+        },
+        })
+        .exec()
+      const allCourses = allCategories.flatMap((category) => category.courses)
+      const mostSellingCourses = allCourses
+        .sort((a, b) => b.sold - a.sold)
+        .slice(0, 10)
+       // console.log("mostSellingCourses COURSE", mostSellingCourses)
+      res.status(200).json({
+        success: true,
+        data: {
+          selectedCategory,
+          differentCategory,
+          mostSellingCourses,
+        },
+      })
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        error: error.message,
+      })
     }
-    catch(err){
-        return res.status(500).json({
-			success: false,
-			message: "Internal server error",
-			error: error.message,
-		});
-    }
-}
+  }
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -9,7 +9,8 @@ import ProfileDropDown from '../core/Auth/ProfileDropDown'
 import { apiConnector } from '../../services/apiConnector'
 import {  categories } from '../../services/api'
 import { useState } from 'react'
-import {IoIosArrowDropdownCircle} from 'react-icons/io';
+import { BiSolidChevronDown } from 'react-icons/bi'
+
 
 const Navbar = () => {
 
@@ -17,94 +18,107 @@ const Navbar = () => {
   // //fetching state
   const {token} = useSelector( (state)=>state.auth);
 
-
-
   const {user} = useSelector( (state)=>state.profile);
   const {totalItems} = useSelector( (state)=>state.cart);
-
+  const [loading, setLoading] = useState(false)
   const [subLinks , setSubLinks] = useState([]);
+  
 
-  const fetchSublinks = async()=>{
-   
-      try{
-        //api calling
-          const result = await apiConnector("GET" ,categories.CATEGORIES_API );
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+      try {
+        const res = await apiConnector("GET", categories.CATEGORIES_API)
        
-          setSubLinks(result.data.data)
-         }
-         catch(err){
-          
-          console.log(err,"unable to fetch categories list")
-         }
-    
-  }
-
-  useEffect( ()=>{
-    fetchSublinks()
-  },[])
+        setSubLinks(res.data.data)
+      } catch (error) {
+        console.log("Could not fetch Categories.", error)
+      }
+      setLoading(false)
+    })()
+  }, [])
 
   const location = useLocation()
   const matchRoute = (route)=>{
     return matchPath( {path:route} , location.pathname);
   }
 
-  return (
-    <div className=' flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700'>
 
-    <div className=' w-11/12 flex max-w-maxContent items-center justify-between'>
+
+  return (
+    <div className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
+      location.pathname !== "/" ? "bg-richblack-800" : ""
+    } transition-all duration-200`}>
+
+    <div className="flex w-11/12 max-w-maxContent items-center justify-between">
 
             <Link to="/">
               <img src={logo} width={160} height={42}/>
             </Link>
 
             <nav>
-              <ul className=' flex gap-x-6 text-richblack-25'>
-                  {NavbarLinks.map( (link , index)=>{
-                    return(
-                      <li key={index} >
-                        {
-                          link.title === "Catalog" ? (
-                            <div className=' flex items-center gap-2 group relative'>
+            <ul className="flex gap-x-6 text-richblack-25">
+            {NavbarLinks.map((link, index) => (
+              <li key={index}>
+                {link.title === "Catalog" ? (
+                  <>
+                    <div
+                      className={`group relative flex cursor-pointer items-center gap-1 ${
+                        matchRoute("/catalog/:catalogName")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      <p>{link.title}</p>
+                      <BiSolidChevronDown />
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                        {loading ? (
+                          <p className="text-center">Loading...</p>
+                        ) : subLinks.length ? (
+                          <>
+                            {subLinks
+                              ?.filter(
+                                (subLink) => subLink?.courses?.length > 0
+                              )
+                              ?.map((subLink, i) => (
+                                <Link
+                                  to={`/catalog/${subLink.name
+                                    .split(" ")
+                                    .join("-")
+                                    .toLowerCase()}`}
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  key={i}
+                                >
                            
-                            <p>{link.title} </p>
-                            <IoIosArrowDropdownCircle/>
-
-                            <div className='invisible absolute left-[50%]
-                                    translate-x-[-50%] translate-y-[50%]
-                                 top-[50%]
-                                flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900
-                                opacity-0 transition-all duration-200 group-hover:visible
-                                group-hover:opacity-100 lg:w-[300px] '>
-
-                             <div className='absolute left-[50%] top-0
-                                translate-x-[80%]
-                                translate-y-[-45%] h-6 w-6 rotate-45 rounded bg-richblack-5'></div>
-                             {
-                                    subLinks.length ? (
-                                            subLinks.map( (subLink, index) => (
-                                                <Link to={`${subLink.link}`} key={index}>
-                                                    <p>{subLink.name}</p>
-                                                </Link>
-                                            ) )
-                                    ) : (<div></div>)
-                                }
-                            </div>
-
-                          </div>
-                         
-                          )
-                          : (
-                            <Link to={link?.path}>
-                             <div className={`${matchRoute(link?.path)? "text-yellow-25" :"text-richblack-25"}`}>{link.title}</div>
-                             </Link>
-                          )
-                          }
-                    </li>
-                     )
-                   
-                  })}
-              </ul>
+                                  <p>{subLink.name}</p>
+                                </Link>
+                              ))}
+                          </>
+                        ) : (
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <Link to={link?.path}>
+                    <p
+                      className={`${
+                        matchRoute(link?.path)
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      {link.title}
+                    </p>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
             </nav>
+          
 
             {/* login signup dashboard */}
 
