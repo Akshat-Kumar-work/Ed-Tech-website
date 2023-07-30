60c3841ea5af289fd1b8d05914dade138d6d437b
Akshat <iamakku0.0.0.1@gmail.com>
2023-07-30 19:15:33 +0530

    buy course page

---
+++ src/pages/CourseDetails.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+const CourseDetails = () => {
+    const {token} = useSelector( (state)=>state.auth)
+
+    const handleBuyCourse = ()=>{
+        if(token){
+            BuyCourse()
+            return;
+        }
+
+
+    }
+
+
+
+  return (
+    <div className='flex items-center justify-center'>
+
+        <button className=' bg-yellow-50' onClick={()=>handleBuyCourse()}>
+            Buy now
+        </button>
+    </div>
+  )
+}
+
+export default CourseDetails
